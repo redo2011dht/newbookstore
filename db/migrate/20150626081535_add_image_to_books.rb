f6985fa0364438b9ed8795a1661d484ed06ddfef
redo2011dht <redo2011dht@gmail.com>
2015-06-26 15:21:44 +0700

    set 2action in db/migrate/20150626081535_add_image_to_books.rb to add and remove image

---
+++ db/migrate/20150626081535_add_image_to_books.rb
@@ -0,0 +1,13 @@
+class AddImageToBooks < ActiveRecord::Migration
+  
+  def self.up
+  	change_table :books do |t|
+  		t.attachment :image
+  	end
+  end
+
+  def self.down
+  	remove_attachment :books, :image
+  end
+
+end
