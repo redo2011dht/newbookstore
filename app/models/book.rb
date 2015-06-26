809077eedc5c8c163445ea9242f3b95e2744b869
redo2011dht <redo2011dht@gmail.com>
2015-06-26 15:24:44 +0700

    set style uploaded images in app/models/book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ActiveRecord::Base
 	belongs_to :publisher
 	belongs_to :category
+
+	has_attached_file :image, styles: { large: "400x600>", medium: "200x300>", thumb: "40x60>" }
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
